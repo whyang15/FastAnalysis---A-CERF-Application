77e19eb62c6ee72406d68780a3f1f0cb3d02a0dc
vjude <107033502+vjude@users.noreply.github.com>
2023-09-08 15:37:08 -0500

    Update flet GUI.py

--- flet GUI.py
+++ flet GUI.py
@@ -1,5 +1,6 @@
 #imported packages for application
 import flet
+import re
 
 #initial coomments:
 #This GUI example will not show other available icons/text until a file has been successfully uploaded.
@@ -14,15 +15,17 @@ import flet
 def main(page: flet.Page):
     #could add application name to title page
     page.title = "Learn about your FASTA file"
+    page.scroll = "adaptive"
     
     #Flet code for selecting a file
     file_picker = flet.FilePicker()
     page.overlay.append(file_picker)
     file = None
     full_path = None
-    #text boxes for diaplaying selected file name and full path
+    #text boxes for displaying selected file name and full path, and analysis results text
     file_name = flet.Text()
     file_path = flet.Text()
+    gd = flet.Text(color = "black")
     
     def on_dialog_result(e:flet.FilePickerResultEvent):
         if e.files != None:
@@ -30,21 +33,26 @@ def main(page: flet.Page):
                 ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
                 )
             #can add other icons for other search results once those have been decided
-            page.add(string_tb, b, t)
+            page.add(string_tb, b, gd)
             file_name.update()
             file = file_name.value
             file_path.value = e.files[0].path
             file_path.update()
             full_path = file_path.value
+            gd.value = "\n Would call functions for other analysis and display results here, for example: \n\n GC content \n\n Taxonomy"
+            gd.update()
+            seq_dictionary = seq_dictionary_generator(file_read(file))
         else:
-            page.add(t)
+            file_path.value = ''
             #add if/else to remove string_tb, b, etc. from the page if no file is selected
-            file_name.value =f"File not available, re-enter"
+            file_name.value =f"File not selected, please select a file to continue."
         page.update()
 
     #icons: file selection
     file_picker = flet.FilePicker(on_result=on_dialog_result)
-    f = flet.ElevatedButton("Upload File", icon=flet.icons.UPLOAD_FILE, on_click=lambda _:file_picker.pick_files(allow_multiple= False, allowed_extensions=['fasta', 'txt']))
+    #button for uploading a file, could place a text field in same row to display name of the CERF imported file??
+    f = flet.Row([flet.ElevatedButton("Upload File", icon=flet.icons.UPLOAD_FILE,
+                                     on_click=lambda _:file_picker.pick_files(allow_multiple= False, allowed_extensions=['fasta', '.fa', 'txt']))])
 
     #def read_file(x):
         #read_file = open(x, 'r')
@@ -64,19 +72,43 @@ def main(page: flet.Page):
     string_tb = flet.TextField(label="Enter header or sequence search:")
     b = flet.ElevatedButton(text="Search", on_click=search_button_clicked)
 
-    #def GC_calcs(e):
-    #icons and variables: GC function
-    #def taxonomy(e):
+    #icons and variables: general display
+    
+    
+    
+    #read the file
+    def file_read(fileName):
+        try:
+            file = open(str(fileName), "r")
+            fileCont = file.readlines()
+            return fileCont
+        except OSError:
+            print("The file '{}' is not found.".format(fileName))
+            return False
+    #icons and variables: read
+    
+    
+    #seq_dictionary function:
+    def seq_dictionary_generator(fileCont):
+    
+        header_position = []
+        file_dictionary = {}
+        
+        for line in str(fileCont):
+            if '>' in line:
+                header_position.append(fileCont.index(line))
+                
+        for pos in header_position:
+            file_dictionary[fileCont[pos]] = ' '.join(map(str,fileCont[pos+1:pos+2]))
+
+        return file_dictionary
+
     #icons and variables: Find taxonomy function
     #etc.
-    
+
     #Add initial file selection icons, other icons are added to the page after file selection (see on_dialog_result)
     page.add(file_picker, file_name, file_path, f, t)
+    #creates sequence dictionary using the uploaded file name
+    
     
 flet.app(target=main)
-
-
-
-
-
-
