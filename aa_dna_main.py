69c01d51c1e46a140e11f41287bc21ae9439eb87
whyang15 <143295436+whyang15@users.noreply.github.com>
2023-09-21 22:35:05 -0700

    Latest modules and functions for integration.

---
+++ aa_dna_main.py
@@ -0,0 +1,147 @@
+# Main function:
+# This script will parse a FASTA file and determine the length of amino acid sequence, 
+# each amino acid content within a sequence, and the hydrophobic / hydrophilic content of sequence.
+
+# import necessary modules
+import sys
+from collections import Counter
+from Bio import SeqIO
+import os
+import fasta_functions as ff
+import aa_functions as aa
+import nuc_functions as nuc
+
+
+# Main function:
+def main():
+    search_results = []
+    search_string = ""
+    num_records = 0
+    rec_num = 0
+    hphobic = 0
+    hphilic = 0
+    gc_pct = 0
+    new_header_sep = "ff output ["
+    formatted_seq=""
+    additional_results=""
+
+    if len(sys.argv) < 2:
+        print("File path not provided.")
+        return
+    
+    filepath = sys.argv[1]
+    print("File path: ", filepath)
+
+    # Search Analysis:
+    # Search is optional.  
+    # If more occurrences are found, add to the positions list.
+    if len(sys.argv) < 3:
+        print("No search word provided.")
+        return
+    else:
+        search_string = sys.argv[2]
+    
+    search_results = ff.search_for_string(filepath, search_string)
+
+    # specify your output directory for search and additional analysis results here.
+    additional_results = filepath.rsplit(".",2)[0]
+    additional_results_filename = additional_results + "_addtional_results.txt"
+    additional_output_path = os.path.join("/Users/Wei-Hsien/Desktop", additional_results_filename)
+    print(additional_output_path)
+
+    
+    # Check whether this FASTA file has already gone through CERF Fasta analysis:
+    is_modified = ff.check_fasta_modified(filepath)
+    if is_modified:
+        # if search string is defined:
+        search_string = sys.argv[2]
+        search_results = ff.search_for_string(filepath, search_string)
+        #print(search_results)
+
+        # open and save results to separate file. 
+        ff.format_additional_results(additional_output_path, search_string, search_results)
+        return
+    
+    else: 
+        # continue with CERF Fasta analysis:
+        # open FASTA file and read in as file:
+        with open(filepath, "r") as file:
+            # get number of records in FASTA file.
+            num_records = ff.get_num_records(filepath)  
+            
+            # parse each record.  
+            for record in SeqIO.parse(file, "fasta"):
+                header = record.description
+                print("current header:  ", header)
+                
+                # continue with aa or dna analysis if not modified:
+                seq=str(record.seq)
+                seqlen=len(seq)
+                rec_num += 1
+                new_header=""
+                print("length of sequence is: ", str(seqlen))
+
+                # check whether FASTA contains nucleotides or amino acids.  then proceed with correct analysis.
+                if ff.is_dna_or_aa(seq, "nucleotides"):
+                    print("This is a nucleotide sequence.")
+                    nuc_counts = nuc.get_dna_counts(seq)
+                    print(nuc_counts)
+
+                    rc = "record_num="+str(rec_num)+"/"+str(num_records)
+                    print(rc)
+                    ln = "seqlen=" + str(seqlen)
+                    nuccounts = "base_counts=" + str(nuc_counts)
+                    at_pct = round(nuc.get_at_counts(seq) / seqlen, 2)
+                    gc_pct = round(nuc.get_gc_counts(seq) / seqlen, 2)
+                    at = "AT%=" + str(at_pct)
+                    gc = "GC%=" + str(gc_pct)
+
+                    new_header_list = [header, "||", new_header_sep, rc, ln, nuccounts, at, gc]
+                    new_header = ' '.join(new_header_list)
+                    print(new_header)
+                    # format new file and write to new FASTA.
+                    formatted_seq += ff.formatFasta(seq, new_header)
+
+                else:
+                    print("This is a peptide sequence.")
+                    aa_counts_line = aa.get_aa_counts(seq)
+                    hphobic += aa.get_hydrophobic_counts(seq)
+                    hphilic += aa.get_hydrophilic_counts(seq)
+
+                    hphobic_pct = round(hphobic / seqlen, 2)
+                    hphilic_pct = round(hphilic / seqlen, 2)
+                    rc = "record_num="+str(rec_num)+"/"+str(num_records)
+
+                    ln = "seqlen=" + str(seqlen)
+                    aacounts = "aa counts=" + str(aa_counts_line)
+                    hl = "hphilic%=" + str(hphilic_pct)
+                    ho = "hphobic%=" + str(hphobic_pct)
+                            
+                    new_header_list = [header, "||", new_header_sep, rc, ln, aacounts, hl, ho, "]"]
+                    new_header = ' '.join(new_header_list)
+                    print(new_header)
+
+                    # format new file and write to new FASTA.
+                    formatted_seq += ff.formatFasta(seq, new_header)
+
+       
+        with open(filepath, "w") as output_file:
+            output_file.write(formatted_seq)
+
+        
+        # if search string is defined:
+        search_string = sys.argv[2]
+        search_results = ff.search_for_string(filepath, search_string)
+        #print(search_results)
+    
+    # specify your output directory for search and additional analysis results here.          
+    ff.format_additional_results(additional_output_path, search_string, search_results)
+
+
+
+
+###-----------------------------------------------------
+main()
+
+
+
---
+++ fasta_functions.py
@@ -0,0 +1,141 @@
+# This module contains functions that checks the input FASTA file for format, content, and previous analysis done.
+from collections import Counter
+#import os
+import re
+from Bio import SeqIO
+
+
+# write function to get the number of records in a FASTA file.
+def get_num_records(filepath):
+    with open(filepath, "r") as file:
+    # get number of records in FASTA file.
+        records = list(SeqIO.parse(file, "fasta"))
+        num_records = len(records)
+        print(f"This fasta contains {num_records} records.")
+        return num_records    
+
+
+# parse each record.  determine whether record has been modified.
+def check_fasta_modified(filepath):
+    new_header_sep = "ff output ["
+    with open(filepath, "r") as file:
+        for record in SeqIO.parse(file, "fasta"):
+            header = record.description
+            print("current header:  ", header)
+            if new_header_sep in header:
+                
+                print("This file has already been modified by CERF Fasta Analysis.")
+                return True
+    return False       
+
+
+''' write a function to read in FASTA.  This function reads in the entire sequence.
+    It returns dnaseq if DNA, aaseq if Amino Acid. '''
+def is_dna_or_aa(sequence, type):
+    # define dictionary set:
+    bases_dict = { 'nucleotides': re.compile('^[ACGTN]*$', re.I),
+               'amino acids': re.compile('^[ACDEFGHIKLMNPQRSTVWY]*$', re.I)}
+    
+    # aa_characters = set("ACDEFGHIKLMNPQRSTVWY")
+    if bases_dict.get(type).search(sequence) is not None:
+        return True
+    else:
+        return False
+
+
+# Write function to add values in 2 different dictionaries together.
+def add_dict_values(dict1, dict2):
+    counter1 = Counter(dict1)
+    counter2 = Counter(dict2)
+    sum_counter = counter1 + counter2
+    sum_counter_dict = dict(sum_counter)
+    return sum_counter_dict
+
+
+# Write function format into FASTA file:
+# Format to Fasta:
+def formatFasta(seq, new_header):
+    header = ""
+    header = ">" + str(new_header) + "\n"
+    
+    # format seq to write 80 nt per line
+    formatted_seq = ""
+    formatted_seq += header
+    for pos in range(0, len(seq), 80):
+        formatted_seq += seq[pos:pos+80]+"\n"
+    # print(formatted_seq)
+    formatted_seq += "\n"
+    return formatted_seq
+
+
+
+# Write a function that searches each sequence for a string.
+# Use sys.args[2] as the search string input.  search_string argument is optional.
+def search_for_string(filepath, search_string=None):
+    # search for occurrences of the string in the sequence and store here:
+    found_seqs = []
+
+    if search_string is None:
+        print("No search word provided.")
+        return found_seqs
+      
+    # covert search string to lower case for case-insensitive search
+    search_string = search_string.lower()
+
+    # open and parse the fasta file. 
+    for record in SeqIO.parse(filepath, "fasta"):
+        header = str(record.description)
+        sequence = str(record.seq)
+
+        # convert sequence to lower case for case-insensitive search
+        sequence = sequence.lower()
+
+        positions = []
+        start_pos = 0
+        while True:
+            pos = sequence.find(search_string, start_pos)
+            if pos == -1:
+                break
+
+            # store the position of the occurrence:
+            positions.append(pos)
+
+            # update the start position of next search:
+            start_pos = pos + 1
+
+        if positions:
+            found_seqs.append({
+                'record': header,
+                'positions': positions
+            })
+
+    return found_seqs
+
+
+# Write a function to reformat search results: 
+def format_additional_results(additional_output_path, search_string, search_results):
+    with open(additional_output_path, "w") as additional_report:
+        output_str = 'Search Results for {} are shown here: '.format(search_string)
+        additional_report.write(output_str)
+
+        for result in search_results:
+            header = result['record']
+            positions = result['positions']
+
+            additional_report.write(f"\nHeader: {header}\n")
+            additional_report.write(f"Number of occurrences: {len(positions)}\n")
+            additional_report.write("Positions: \n")
+
+            for pos in positions:
+                additional_report.write(str(pos) + "\n")
+                
+            additional_report.write("------------------\n")
+
+
+    
+
+
+
+
+
+  
