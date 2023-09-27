7cd81dcb9c4e84fb5a0ec077ba60afe6d5cf69a0
vjude <107033502+vjude@users.noreply.github.com>
2023-09-26 23:00:45 -0500

    Update GUI.py

--- GUI + Analysis scripts/GUI.py
+++ GUI + Analysis scripts/GUI.py
@@ -11,39 +11,23 @@ import aa_dna_main as aa
 #currently the search function can be entered in lowercase or capitals, FASTA can be in either, so when writing the backend code may need to account for either case
 #The full path of the uploaded file is saved to the variable 'full_path'
 
-def file_read(fileName):
-        try:
-            open_file = open(fileName)
-            fileCont = open_file.read() 
-            return fileCont
-        except OSError:
-            print("The file '{}' is not found.".format(fileName))
-            return False
-
 def main(page: flet.Page):
     
-    #could add application name to title page
     #main page header
-    page.title = "Learn about your FASTA file"
+    page.title = "FastAnalysis - A CERF Application"
     #page scrolling
     page.scroll = "adaptive"
     #main page background color
     page.bgcolor = flet.colors.BLUE_100
-    
-    #Flet code for selecting a file, if file selection successful, file is read and sequences
-    #will be placed in a dictionary using seq_dictionary() function
-    file = None
 
     #text boxes for displaying selected file name and full path, and analysis results text
-    #may not need file_name box anymore
     file_name = flet.Text()
     file_path = flet.Text()
     gd = flet.Text(color = "black")
 
     #accepted FASTA  file extensions
-    ext = ["faa", "fa", "fasta", "fna", "FAA", "FA", "FASTA", "FNA"]
+    ext = ["faa", "fa", "fasta", "fna"]
 
-    results = ""
     formatted_seq = ""
     
     #must remain embedded within main(), when a file is selected by the user using the Upload button the file path is read in and displayed to the text boxes
@@ -51,40 +35,49 @@ def main(page: flet.Page):
         if e.files != None:
             #can add other icons for other search results once those have been decided
             modified, message, formatted_seq_Wallace, formatted_seq_GC, formatted_seq_NN, formatted_seq = aa.pass_file(e.files[0].path)
-            page.add(string_tb, b, gd, c)
+            
+            #w and GC text fields are placeholders for values, they are also inclued in the page to add spacing between icons ven though they do not display any data
+            page.add(tm_dropdown, NN, string_tb, b, w, re_enzyme_search, re, GC, ORF, gd, c)
             file_path.value = e.files[0].path
             file_path.update()
-            #file = file_read(file_path.value)
+            
             #save the TM values (optional test) to values outside of the function, only needed when file is not passed in as an argument
             #t.value holds the file with no TM included
             t.value = formatted_seq
-            t.update()
             #GC.value holds the updated file contents with TM calculated using GC method
             GC.value = formatted_seq_GC
-            #GC.update()
             #w.value holds the updated file with TM calculated using Wallace method
             w.value = formatted_seq_Wallace
-            #w.update()
             #NN.value holds the file contents with the TM calculated using NN mothod
             NN.value = formatted_seq_NN
-            #NN.update()
-            #gd.value holds returned statement of whether file has been modified already or not
+            #file_name.value holds returned statement of whether file has been modified already or not, and whether the file contains protein or nucleotide seqs
+            file_name.value = message
+            file_name.update()
             gd.value = "\n\nAnalysis results: \n"
             gd.update()
-            #return modified, message, formatted_seq
         else:
             file_path.value = ''
             file_path.update()
+            #could get rid of file_name box if find a new location for error message?
             file_name.value = "File not selected, select a file to continue"
             file_name.update()
-        #return modified, message
-        page.update()
 
     #function to take in the string to be searched for and do something with it
     def search_button_clicked(e):
-            output_file_path= aa.searching(file_path.value, string_tb.value)
-            t.value = f"File containing output can be found at: " + output_file_path
-            t.update()
+        output_file_path = aa.searching(file_path.value, string_tb.value)
+        file_name.value = "Results of your search can be found at: " + output_file_path
+        file_name.update()
+
+    #perform restriction enyme search using user entered string
+    def re_enzyme_search_clicked(e):
+        message = aa.restriction_enzyme(file_path.value, re_enzyme_search.value, str(tm_dropdown.value))
+        file_name.value = message
+        file_name.update()
+
+    def generate_ORF_file(e):
+        message = aa.calculate_ORFs(file_path.value, "", str(tm_dropdown.value))
+        file_name.value = message
+        file_name.update()
 
     #icons and text boxes
     #containers for tm values
@@ -94,11 +87,19 @@ def main(page: flet.Page):
     NN = flet.Text()
 
     #icons
-    string_tb = flet.TextField(label="Enter header or sequence search:")
+    string_tb = flet.TextField(label="Enter string search:")
     b = flet.ElevatedButton(text="Search", icon=flet.icons.SEARCH, on_click=search_button_clicked)
     file_picker = flet.FilePicker(on_result=on_dialog_result)
     f = flet.Row([flet.ElevatedButton("Upload File", icon=flet.icons.UPLOAD_FILE,
-                                         on_click=lambda _:file_picker.pick_files(allow_multiple= False, allowed_extensions=['fasta', 'fa', 'faa', 'fna']))])
+                                         on_click=lambda _:file_picker.pick_files(allow_multiple= False, allowed_extensions= ext))])
+
+
+    #restriction enzyme search field
+    re_enzyme_search = flet.TextField(label="Enter the Restriction enzyme search: You can enter a list of restriction enzymes to search for, or look through restriction enzyme database (All or Common).")
+    re = flet.ElevatedButton(text="Search", icon=flet.icons.SEARCH, on_click=re_enzyme_search_clicked)
+
+    #button for generating ORF data for the file
+    ORF = flet.ElevatedButton(text="Generate ORF File", icon=flet.icons.FILE_DOWNLOAD_ROUNDED, on_click=generate_ORF_file)
 
 
     #function that's launched after the Commit button is seleted, this will write the data back to the header
@@ -111,7 +112,6 @@ def main(page: flet.Page):
                     #If a different Tm is selected and the commit icon is selected again (same file) the file's contents are updated and the Tm values are replaced.
                     if tm_dropdown.value == "Tm_None" or tm_dropdown.value == None:
                             aa.commit_results(file_path.value, t.value)
-                            tm_included = False
                     elif tm_dropdown.value == "Tm_Wallace":
                             aa.commit_results(file_path.value, w.value)
                     elif tm_dropdown.value == "Tm_GC":
@@ -138,21 +138,21 @@ def main(page: flet.Page):
             
     #An info button will be displayed along with the commit button, when selected it will display a container at the bottom of the page.
     #The container will display the information that will be written back to the file if the user decides to select commit
-    tm_dropdown = flet.Dropdown(bgcolor = flet.colors.WHITE, label="Select Tm", hint_text="Should Tm be included in the header?",
+    tm_dropdown = flet.Dropdown(bgcolor = flet.colors.WHITE, label="Select Tm method:", hint_text="Should Tm be included in the header?",
                                 options=[flet.dropdown.Option("Tm_None"),flet.dropdown.Option("Tm_Wallace"),flet.dropdown.Option("Tm_GC"), flet.dropdown.Option("Tm_NN"),],)
-    c = flet.Row([tm_dropdown, flet.ElevatedButton(text="Commit Results", on_click=commit_clicked), flet.ElevatedButton("Info", icon=flet.icons.INFO_OUTLINED, on_click=show_bs_click)], alignment=flet.MainAxisAlignment.END, vertical_alignment=flet.CrossAxisAlignment.END)
+    c = flet.Row([flet.ElevatedButton(text="Commit Results", on_click=commit_clicked), flet.ElevatedButton("Info", icon=flet.icons.INFO_OUTLINED, on_click=show_bs_click)], alignment=flet.MainAxisAlignment.END, vertical_alignment=flet.CrossAxisAlignment.END)
 
     #Potential statement for using sys.argv to check whether the last command line statement was passing in a file of a valid extension [ext]
     #first check to see whether an argument was passed in from the command line (sys.argv[0] = full path to script, if an argument was passed it will be in sys.argv[1])
     length = len(sys.argv)
     if length>1 and sys.argv[1].endswith(tuple(ext)):
         #could get rid of code for reading in file, and read_file function as this is performed in the analysis code?
-        #file = file_read(sys.argv[1])
         modified, message, formatted_seq_Wallace, formatted_seq_GC, formatted_seq_NN, formatted_seq= aa.pass_file(sys.argv[1])
-        page.add(file_picker, file_name, file_path, f, t, string_tb, b, gd, c)
+        #w and GC text fields are placeholders for values, they are also inclued in the page to add spacing between icons ven though they do not display any data
+        page.add(file_picker, file_name, file_path, f, t, string_tb, b, w, re_enzyme_search, re, GC, ORF, gd, c)
         #for now displays the files contents
         t.value = formatted_seq
-        t.update()
+        #t.update()
         #GC.value holds the updated file contents with TM calculated using GC method
         GC.value = formatted_seq_GC
         #w.value holds the updated file with TM calculated using Wallace method
@@ -163,7 +163,7 @@ def main(page: flet.Page):
         file_path.value = os.path.abspath(sys.argv[1])
         file_path.update()
         #could potentially change GUI so application description is displayed in the GD text field
-        gd.value = "\n\n Analysis results will display here"
+        gd.value = message + "\n\n Analysis results will display here: \n"
         gd.update()
     #if no argument is passed in, will only display the Upload file icon prompting the user to select an appropriate file
     else:
