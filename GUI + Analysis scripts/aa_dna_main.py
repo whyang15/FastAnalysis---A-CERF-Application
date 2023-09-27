04ed25e450ffc950c1212aa7b0fa5563024a2ce4
vjude <107033502+vjude@users.noreply.github.com>
2023-09-26 23:02:49 -0500

    Update aa_dna_main.py

--- GUI + Analysis scripts/aa_dna_main.py
+++ GUI + Analysis scripts/aa_dna_main.py
@@ -10,6 +10,10 @@ import pathlib
 from collections import Counter
 from Bio import SeqIO
 from Bio.SeqUtils import MeltingTemp as mt
+from Bio.Seq import Seq
+from Bio.Restriction import * 
+from Bio.Restriction.PrintFormat import PrintFormat
+from Bio.SeqRecord import SeqRecord
 
 import fasta_functions as ff
 import aa_functions as aa
@@ -35,7 +39,6 @@ def pass_file(filepath):
     with open(filepath, "r") as file:
         for record in SeqIO.parse(file, "fasta"):
             header = record.description
-            print("current header:  ", header)
             if new_header_sep in header:
                 modified = True
                 message = str("This file has already been modified by CERF Fasta Analysis.")
@@ -43,7 +46,7 @@ def pass_file(filepath):
                 break
             else:
                 modified = False
-                message = str("This file has not yet been analyzed")
+                message = str("This file has not yet been analyzed. ")
             # continue with aa or dna analysis if not modified:
             seq=str(record.seq)
             seqlen=len(seq)
@@ -85,15 +88,8 @@ def pass_file(filepath):
                 formatted_seq_NN += ff.formatFasta(seq, NN_header)
                 formatted_seq += ff.formatFasta(seq, new_header)
 
-                
-                #print(Wallace_header, GC_header, NN_header, new_header)
-                
-                # format new file based on TM methods
-##                formatted_seq_Wallace += ff.formatFasta(seq, Wallace_header)
-##                formatted_seq_GC += ff.formatFasta(seq, GC_header)
-##                formatted_seq_NN += ff.formatFasta(seq, NN_header)
-##                formatted_seq += ff.formatFasta(seq, new_header)
-                
+                message =str('\n\n' + message + "This FASTA file contains nucleotides sequences. \nSelect a Tm method from the dropdown. If Tm is selected, Tm will be included in restriction enzyme search and committed data.")
+
             else:
                 aa_counts_line = aa.get_aa_counts(seq)
                 hphobic += aa.get_hydrophobic_counts(seq)
@@ -113,7 +109,10 @@ def pass_file(filepath):
 
                 # format new file and write to new FASTA.
                 formatted_seq += ff.formatFasta(seq, new_header)
-        print(formatted_seq)
+                formatted_seq_Wallace = formatted_seq
+                formatted_seq_GC = formatted_seq
+                formatted_seq_NN = formatted_seq
+                message = str('\n\n' + message + "This file contains protein sequences. Tm is not performed on protein FASTA files. \n\nIf a Tm is selected from the dropdown and the file contains amino acids sequences, Tm will not be included in the header.")
                     
     return modified, message, formatted_seq_Wallace, formatted_seq_GC, formatted_seq_NN, formatted_seq
     
@@ -122,30 +121,192 @@ def commit_results(filepath, data):
     myfile.write(data)
     myfile.close()
 
-def searching(filepath, string):
-    #this works on Windows only, potentially Mac?
+def make_results_folder(filepath, string, extension):
+    
+    #this should work on any OS (perk of using pathlib), but should test (so far works on Windows)
+    #FastAnalysis Results folder will contian all results from all searches and ORF generations from the user
+    folder = r'FastAnalysis_Results'
     desktop = pathlib.Path.home()/'Desktop'
-    count = 0
+    desktop_folder = os.path.join(desktop, folder)
 
-    #filepath is passed in from the GUI.
-    #string will be the users search, passed in from the GUI
-    search_results = []
+    #if this is the first search ever performed, the desktop folder to hold the results needs to be created first.
+    if os.path.exists(desktop_folder) == False:
+        os.makedirs(desktop_folder)
 
     # specify your output directory for search and additional analysis results here.
+    #Finds the basename of the file from the full path, this can then be included in the name of the file containing the search results
     additional_results = os.path.basename(filepath)
-    additional_results_filename = (os.path.splitext(additional_results)[0]) + "_addtional_results.txt"
+
+    #Each search will include the string searched for in the file name. If the same string is searched on the same file, the file will overwrite the existing files contents.
+    #By including the searched string in the name, this should make the results more easily findable by CERF once a system has been developed for adding the results as an attachment.
+    additional_results_filename = (os.path.splitext(additional_results)[0]) + "_" + string + extension
+    additional_output_path = os.path.join(desktop_folder, additional_results_filename)
+
+    return additional_output_path
     
-    #include if/else statement to see whether a a file already exists, so files dont become overwritten every time a new search is performed?
-    #This would allows users to perform multiple searches, and each search would have its own file.
-    additional_output_path = os.path.join(desktop, additional_results_filename)
-    #if a search has already been performed and a file has been written to the desktop, an new file with a +1 count in the name will be added to the desktop
-    #work on bug
-    if os.path.exists(additional_output_path):
-        count += 1
-        additional_results_filename = (os.path.splitext(additional_results)[0]) + "_" + str(count) +"_addtional_results.txt"
-        additional_output_path = os.path.join(desktop, additional_results_filename)
+    
+def searching(filepath, string):
+    #filepath is passed in from the GUI.
+    #string will be the users search, passed in from the GUI
+    #call method for creating the results folder if needed, returns full path of the file that results will be written to, will include the string in the file name
+    extension = "_addtional_results.txt"
+    additional_output_path = make_results_folder(filepath, string, extension)
+    search_results = []
 
     search_results, search_string = ff.search_for_string(filepath, string)
     ff.format_additional_results(additional_output_path, search_string.upper(), search_results)
-    
+
+    #return path to result file, so user knows where it will be located
     return additional_output_path
+
+def restriction_enzyme(filepath, string, tm_dropdown):
+    #If nothing is entered in the search bar but the button is selected, then an error message will display to the user
+    if string == "" or None:
+        message = str("Sequence not entered: Enter a valid sequence to search.")
+        return message
+    
+    #call method for creating the results folder if needed, returns full path of the file that results will be written to, will include the string in the file name
+    extension = "_reCut_results.txt"
+    additional_output_path = make_results_folder(filepath, string, extension)
+    re_results_output = ""
+    
+    new_header_sep = "ff output ["
+    rec_num = 0
+    num_records = 0
+    
+
+    with open(filepath, "r") as file:
+        #parse through the FASTA file using BioPython
+        for record in SeqIO.parse(file, "fasta"):
+
+            seq=str(record.seq)
+            
+            #restriction enzyme sites are only found on nucleotides sequences, if user has uploaded a FASTA file containing amino acid sequences will not continue.
+            if ff.is_dna_or_aa(seq, "nucleotides"):
+                
+                header = record.description
+                
+                
+                #if the file has already had it's header updated, then no need to perform the analysis to make the new header.
+                #Can just use the existing header for the search results. This allows restriction enzyme sites to still be searched for, regardless of whether the file's header has been modified.
+                if new_header_sep in header:
+                    new_header = header
+                else:
+                
+                    seqlen=len(seq)
+                    nuc_counts = nuc.get_dna_counts(seq)
+                    rec_num += 1
+                    
+                    #general base counts and calcs to be included in the header
+                    rc = "record_num="+str(rec_num)+"/"+str(num_records)
+                    ln = "seqlen=" + str(seqlen)
+                    nuccounts = "base_counts=" + str(nuc_counts)
+                    at_pct = round(nuc.get_at_counts(seq) / seqlen, 1)
+                    gc_pct = round(nuc.get_gc_counts(seq) / seqlen, 1)
+                    at = "AT%=" + str(at_pct*100)
+                    gc = "GC%=" + str(gc_pct*100)
+
+                    #tm calc, will be included automatically in the header data if a Tm method is selected in the dropdown menu
+                    #if no selection is made or a "Tm_None" is selected, Tm will not be included in the reulsts file
+                    if tm_dropdown == "Tm_Wallace" or tm_dropdown == "Tm_GC" or tm_dropdown == "Tm_NN":
+                        tm = nuc.cal_tm(seq, tm_dropdown)
+                        tm_string = "Tm(" + tm_dropdown + ")=" + str(tm) + "'C"
+                        new_header_list = [header, "||", new_header_sep, rc, ln, nuccounts, at, gc, tm_string]
+                    else:
+                        new_header_list = [header, "||", new_header_sep, rc, ln, nuccounts, at, gc]
+                    
+                    new_header = ' '.join(new_header_list)
+
+                #Restriction enzymes
+                enzyme_names, positions_list, frags_list = nuc.reCut(seq, string)
+                
+                # accumulate the formatted results:
+                re_results_output = re_results_output + ff.format_re_results(enzyme_names, positions_list, frags_list, new_header)
+
+                #write the results of the search to the new file
+                commit_results(additional_output_path, re_results_output)
+
+                #return the location of the results (full path)
+                message = str("The results of your search can be found at: " + additional_output_path)
+                
+            #restriction enzyme sites are only found on nucleotides sequences, if user has uploaded a FASTA file containing protein sequences will not search for protein seqs.
+            else:
+                message = str("File contains protein sequences. Restriction enzymes sites are found in nucleotide sequences.")
+                return message
+            
+    return message
+
+def calculate_ORFs(filepath, string, tm_dropdown):
+
+    #call method for creating the results folder if needed, returns full path of the file that results will be written to, will include the string in the file name
+    extension = "_ORF_results.txt"
+    additional_output_path = make_results_folder(filepath, string, extension)
+    all_orf_records = []
+    orf_records =[]     # initialize empty record to store outputs.
+    
+    new_header_sep = "ff output ["
+    rec_num = 0
+    num_records = 0
+
+    #turn into a seperate method?
+    with open(filepath, "r") as file:
+        #parse through the FASTA file using BioPython
+        for record in SeqIO.parse(file, "fasta"):
+
+            seq=str(record.seq)
+            
+            #restriction enzyme sites are only found on nucleotides sequences, if user has uploaded a FASTA file containing amino acid sequences will not continue.
+            if ff.is_dna_or_aa(seq, "nucleotides"):
+                
+                header = record.description
+                
+                #if the file has already had it's header updated, then no need to perform the analysis to make the new header.
+                #Can just use the existing header for the search results. This allows restriction enzyme sites to still be searched for, regardless of whether the file's header has been modified.
+                if new_header_sep in header:
+                    new_header = header
+                else:
+                
+                    seqlen=len(seq)
+                    nuc_counts = nuc.get_dna_counts(seq)
+                    rec_num += 1
+                    
+                    #general base counts and calcs to be included in the header
+                    rc = "record_num="+str(rec_num)+"/"+str(num_records)
+                    ln = "seqlen=" + str(seqlen)
+                    nuccounts = "base_counts=" + str(nuc_counts)
+                    at_pct = round(nuc.get_at_counts(seq) / seqlen, 1)
+                    gc_pct = round(nuc.get_gc_counts(seq) / seqlen, 1)
+                    at = "AT%=" + str(at_pct*100)
+                    gc = "GC%=" + str(gc_pct*100)
+
+                    #tm calc, will be included automatically in the header data if a Tm method is selected in the dropdown menu
+                    #if no selection is made or a "Tm_None" is selected, Tm will not be included in the reulsts file
+                    if tm_dropdown == "Tm_Wallace" or tm_dropdown == "Tm_GC" or tm_dropdown == "Tm_NN":
+                        tm = nuc.cal_tm(seq, tm_dropdown)
+                        tm_string = "Tm(" + tm_dropdown + ")=" + str(tm) + "'C"
+                        new_header_list = [header, "||", new_header_sep, rc, ln, nuccounts, at, gc, tm_string]
+                    else:
+                        new_header_list = [header, "||", new_header_sep, rc, ln, nuccounts, at, gc]
+                    
+                    new_header = ' '.join(new_header_list)
+
+                #ORFs
+                orf_records = nuc.find_orfs(seq)
+                
+                # accumulate the formatted results:
+                orf_records_string = ">" + new_header + "\n" + "".join(orf_records)
+                all_orf_records.append(orf_records_string)
+                all_orf_records_string = "\n".join(all_orf_records)
+
+                #write the results of the search to the new file
+                commit_results(additional_output_path, all_orf_records_string)
+
+                #return the location of the results (full path)
+                message = str("The results of your search can be found at: " + additional_output_path)
+                
+            #ORFs are only found on nucleotides sequences, if user has uploaded a FASTA file containing protein sequences will not search for protein seqs.
+            else:
+                message = str("File contains amino acid sequences. ORFs are found in nucleotide sequences.")
+                return message
+            
+    return message
